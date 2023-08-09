16d7e74edc9d5ad7f13ba52304d66f8317bad01d
TILIN89 <140836235+TILIN89@users.noreply.github.com>
2023-08-08 18:31:22 -0600

    Update audios.js

--- plugins/audios.js
+++ plugins/audios.js
@@ -3,7 +3,7 @@ handler.all = async function (m) {
 let chat = global.db.data.chats[m.chat]
 
 if (/^hola|Holis|olaHola|holis$/i.test(m.text) && chat.audios && !chat.isBanned) {
-let vn = './views/Hola.mp3'
+let vn = './views/hola (1).mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
 

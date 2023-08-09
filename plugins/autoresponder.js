47debd45f68c78d2ce9a96611eb32a6de52c8a16
TILIN89 <140836235+TILIN89@users.noreply.github.com>
2023-08-08 19:30:50 -0600

    Update autoresponder.js

--- plugins/autoresponder.js
+++ plugins/autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: 'ꨄ︎𝙷𝙰𝚃𝚂𝚄𝙽𝙴-𝙼𝙸𝙺𝚄-𝚄𝙻𝚃𝚁𝙰ꨄ︎', body: '©ᴏғᴄ-ʏᴏᴠᴀɴɪ﹏✍', sourceUrl: `https://github.com/OFC-YOVANI/HADES-BOT-MD`, thumbnail: imagen2}}})}
+contextInfo: { externalAdReply: { title: 'ꨄᴬˣᵉˡ ᵒᶠᶜꨄ︎', body: 'ᴬˣᵉˡ ᵒᶠᶜ✍', sourceUrl: `https://github.com/OFC-YOVANI/HADES-BOT-MD`, thumbnail: imagen2}}})}
     
 return !0 }
 handler.register = true
